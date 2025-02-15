811f611bac9d31b4ee114cf20175fdfa5100c45e
Kunjal Arora <arorak2024@gmail.com>
2025-02-15 18:21:20 -0500

    error with router

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,39 +1,92 @@
 "use client";
 import { useState, ChangeEvent, useEffect } from "react";
 import axios from "axios";
+import { useRouter } from 'next/router';
 
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
-import { green, lightGreen } from "@mui/material/colors";
 import Typography from '@mui/material/Typography';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Button from '@mui/material/Button';
 
 const style = {
-    position: "absolute",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 500, 
-    bgcolor: "background.paper",
-    boxShadow: 24,
-    borderRadius: 2,
-    p: 3, 
+  position: "absolute",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500, 
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: 2,
+  p: 3, 
 };
 
 export default function MultilineTextFields() {
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userPhoto, setUserPhoto] = useState("");
+  // const [isClient, setIsClient] = useState(false); 
+  // const [router, setRouter] = useState<any>(null);
   
   useEffect(() => {
     document.body.style.backgroundColor = "#91BA8d"; 
+    // setIsClient(true); 
+    // setRouter(useRouter());
     return () => {
       document.body.style.backgroundColor = ""; 
     };
   }, []);
+  
+  const createUser = () => {
+    axios
+      .post("http://localhost:3000/api/users", {
+        name: userName,
+        email: userEmail,
+        password: userPassword,
+        photo: userPhoto,
+      })
+      .then(function (response) {
+        console.log(response);
+        setUserName("");
+        setUserEmail("");
+        setUserPassword("");
+        setUserPhoto("");
+
+        // Redirect to the home page after success
+        // if (isClient) {
+        //   router.push("/");
+        // }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const getUser = () => {
+    // Define what happens on login (similar to createUser)
+    console.log("Login clicked");
+  };
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserName(e.target.value);
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserPassword(e.target.value);
+  };
+
+  const handlePhotoChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserPhoto(e.target.value);
+  };
 
   return (
     <Stack sx={style} spacing={3}> 
@@ -44,6 +97,8 @@ export default function MultilineTextFields() {
         label="Your Name"
         multiline
         maxRows={4}
+        value={userName}
+        onChange={handleNameChange}
         sx={{ marginBottom: 2, width: '100%' }} 
       />
       
@@ -52,6 +107,8 @@ export default function MultilineTextFields() {
         label="Email Address"
         multiline
         maxRows={4}
+        value={userEmail}
+        onChange={handleEmailChange}
         sx={{ marginBottom: 2, width: '100%' }} 
       />
       
@@ -60,6 +117,8 @@ export default function MultilineTextFields() {
         label="Password"
         multiline
         maxRows={4}
+        value={userPassword}
+        onChange={handlePasswordChange}
         variant="filled"
         sx={{ marginBottom: 2, width: '100%' }}
       />
@@ -69,14 +128,15 @@ export default function MultilineTextFields() {
         label="Photo"
         multiline
         rows={4}
-        defaultValue="null"
+        value={userPhoto}
+        onChange={handlePhotoChange}
         variant="filled"
         sx={{ marginBottom: 2, width: '100%' }} 
       />
 
       <ButtonGroup disableElevation variant="contained" aria-label="Disabled button group">
-        <Button>Signup</Button>
-        <Button>Login</Button>
+        <Button onClick={createUser}>Signup</Button>
+        <Button onClick={getUser}>Login</Button>
       </ButtonGroup>
     </Stack>
   );
