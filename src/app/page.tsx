664326c6ff8ac60ff7a426af65d87ea02cb09b35
Kunjal Arora <arorak2024@gmail.com>
2025-02-15 10:14:59 -0500

    boxing content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap is imported
 import { Button } from "@mui/material";
 
-export default function Home() {
+function Example() {
   return (
-    <div>
-      <Button variant="contained">Get your chores done</Button>
-    </div>
-  )
+    <Container>
+      <Row className="roof">
+        <Col className="text-center p-3 border">
+          <span>roof+functions come here</span>
+          <div>
+            <Button variant="contained">Get your chores done</Button>
+            </div>
+        </Col>
+      </Row>
+      <Row className="base">
+        <Col sm={4} className="text-center p-5 border">
+          <span>One of three columns</span>
+        </Col >
+        <Col sm={4} className="text-center p-5 border">
+          <span>One of three columns</span>
+        </Col>
+        <Col sm={4} className="text-center p-5 border">
+          <span>One of three columns</span>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
+
+export default Example;
+
+
