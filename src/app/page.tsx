1fbd3524c15211b7fc2c27dda9069bb1d6848de3
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2025-02-14 23:50:20 -0500

    fix error hopefully?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Button from "@mui/material/Button";
 
 export default function Home() {
-  return <Button variant="contained">Get your chores done</Button>;
+  return (
+    <div>
+      <Button variant="contained">Get your chores done</Button>
+    </div>
+  )
 }
