ad1de32a721e49e8d26bddf216f4161648a9c048
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2025-02-15 14:55:47 -0500

    image works!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,36 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material"; // Importing Material UI components
+import { Box, Button, Typography } from "@mui/material";
 import TaskWidget from "./components/TaskWidget";
-
-import Image, { StaticImageData } from "next/image";
-import image from '../public/img/images.jpeg';
+import Image from "next/image";
 
 export default function House() {
   return (
     <Box className="container">
       <Box className="roof">
-
-      <Box className="triangle">
-            <span className="triangle-text">
-            <Box className="circle">.</Box>
+        <Box className="triangle">
+          <span className="triangle-text">
+            <Box className="circle">
+              <Image
+                src="/img/images.jpeg"
+                alt="Jeremy, the coolest roommate ever"
+                layout="fill"
+                objectFit="cover"
+                style={{ borderRadius: "50%" }}
+              />
+            </Box>
             <div>
               <Button variant="contained">+</Button>
-              </div>
-            </span>
-          </Box>
+            </div>
+          </span>
+        </Box>
       </Box>
       <Box className="base">
-      <div className="grid">
-
-        {/* Column 1 */}
-        <div className="column">
-          <Typography variant="body1">
-            One of three columns
-          </Typography>
-          <TaskWidget/>
-        </div>
+        <div className="grid">
+          {/* Column 1 */}
+          <div className="column">
+            <Typography variant="body1">One of three columns</Typography>
+            <TaskWidget />
+          </div>
 
           {/* Column 2 */}
           <div className="column">
@@ -42,5 +44,5 @@ export default function House() {
         </div>
       </Box>
     </Box>
-  )
+  );
 }
