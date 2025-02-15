4d2e3bb7b158fcd6fcae6e2948a52d0348343547
Katarina Vucic <kavucic@icloud.com>
2025-02-15 01:50:48 -0500

    import from pre-generated prisma client

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
-import { PrismaClient } from '@prisma/client';
+import prisma from '../../utils/prisma-client';
 
 const router = Router();
-const prisma = new PrismaClient();
 
 router.get('/users', async (req, res) => {
 
